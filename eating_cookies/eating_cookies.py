9b5956349a7fb4008aee960f9922ed3c2ca5d40b
allisonkydy <allisnelly@gmail.com>
2020-01-22 16:58:23 -0600

    eating cookies cache attempt wip

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,12 +1,28 @@
-#!/usr/bin/python
+# !/usr/bin/python
 
 import sys
 
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+
 def eating_cookies(n, cache=None):
-    # base cases
+    # cache solution
+    # if n < 0:
+    #     return 0
+
+    # if cache is None:
+    #     cache = [1, 1, 2]
+    # # elif cache[0] == 0:
+    # #     cache[0] = 1
+    # #     cache[1] = 1
+    # #     cache[2] = 2
+
+    # if n not in cache:
+    #     cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+    # return cache[n]
+
+    # non cache solution
     if n < 0:
         return 0
     elif n < 2:
