6a0a584e829783ef8c3952a3d57d94cf30c15b83
allisonkydy <allisnelly@gmail.com>
2020-01-22 20:12:20 -0600

    complete eating cookies cache solution

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,27 +8,29 @@ import sys
 
 def eating_cookies(n, cache=None):
     # cache solution
-    # if n < 0:
-    #     return 0
-
-    # if cache is None:
-    #     cache = [1, 1, 2]
-    # # elif cache[0] == 0:
-    # #     cache[0] = 1
-    # #     cache[1] = 1
-    # #     cache[2] = 2
-
-    # if n not in cache:
-    #     cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
-    # return cache[n]
-
-    # non cache solution
     if n < 0:
         return 0
-    elif n < 2:
-        return 1
-    else:
-        return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
+
+    if cache is None:
+        cache = [0 for i in range(n + 1)]
+    
+    cache[0] = 1
+    if n > 0:
+        cache[1] = 1
+    if n > 1:
+        cache[2] = 2
+
+    if cache[n] == 0:
+        cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
+    return cache[n]
+
+    # non cache solution
+    # if n < 0:
+    #     return 0
+    # elif n < 2:
+    #     return 1
+    # else:
+    #     return eating_cookies(n - 1) + eating_cookies(n - 2) + eating_cookies(n - 3)
 
 
 if __name__ == "__main__":
